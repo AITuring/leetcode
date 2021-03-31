98ea860e1d1ee09172d3b7952e1a5cddfab3d1bf
AITuring <shspku365@gmail.com>
2021-03-31 22:31:09 +0800

    78 subs(⊙o⊙)…t

---
+++ 78. Subsets/iteration.js
@@ -0,0 +1,18 @@
+var subsets = function (nums) {
+    const ans = []
+    const n = nums.length
+    for(let mask = 0;mask < (1 << n);++mask){
+        const t = []
+        for(let i = 0;i < n; i++) {
+            if (mask & (1 << i)){
+                t.push(nums[i])
+            }
+        }
+        ans.push(t)
+    }
+    return ans
+    
+}
+
+nums = [1,2,3]
+console.log(subsets(nums))
